b360d880a731ef06890751f576cad3c4a1e6210d
Andrei Pieleanu <andrei.pieleanu@admins-MacBook-Pro.local>
2026-04-15 16:11:42 +0300

    added suspense page

---
+++ app/confirm/inner.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+
+const API = "http://localhost:8080";
+
+export default function ConfirmPageInner() {
+    const searchParams = useSearchParams();
+    const [message, setMessage] = useState("Confirming...");
+
+    const token = searchParams.get("token");
+
+    useEffect(() => {
+        if (!token) return;
+
+        fetch(`${API}/auth/confirm?token=${token}`)
+            .then(res => res.text())
+            .then(data => setMessage(`${data}. You can close this window now.`))
+            .catch(() => setMessage("Error confirming account"));
+    }, [token]);
+
+    return <h2>{message}</h2>;
+}
--- app/confirm/page.tsx
+++ app/confirm/page.tsx
@@ -1,21 +1,12 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { useSearchParams } from "next/navigation";
-
-const API = "http://localhost:8080";
+import { Suspense } from "react";
+import ConfirmPageInner from "@/app/confirm/inner";
 
 export default function ConfirmPage() {
-    const searchParams = useSearchParams();
-    const [message, setMessage] = useState("Confirming...");
-    const token = searchParams.get("token");
-
-    useEffect(() => {
-        fetch(`${API}/auth/confirm?token=${token}`)
-            .then(res => res.text())
-            .then(data => setMessage(`${data}. You can close this window now.`))
-            .catch(() => setMessage("Error confirming account"));
-    }, [token]);
-
-    return <h2>{message}</h2>;
+    return (
+        <Suspense fallback={<h2>Loading...</h2>}>
+            <ConfirmPageInner />
+        </Suspense>
+    );
 }
